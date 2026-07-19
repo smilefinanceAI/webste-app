cc438e9b86fd69663e380aa8be8f66fe4411f00d
smilefinanceAI <aarifkhankhatri00786@gmail.com>
2026-07-19 14:17:35 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyArcvyvNAOKipPZB0YEfSeoeN9_k-8JznQ",
   authDomain: "smile-business-os.firebaseapp.com",
   projectId: "smile-business-os",
@@ -13,12 +7,3 @@ const firebaseConfig = {
   appId: "1:114356400630:web:96fc9d07baaf3a504c17d7",
   measurementId: "G-73WD9V05ZZ"
 };
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const analytics = getAnalytics(app);
-
-export { app, auth, db, storage, analytics };
