e8581c1af238ad2f8aada8c7e5a1a6570be5475b
smilefinanceAI <aarifkhankhatri00786@gmail.com>
2026-07-19 14:18:08 +0530

    Update firebase-service.js

--- js/firebase-service.js
+++ js/firebase-service.js
@@ -1,17 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-
 export const auth = getAuth(app);
-
-export const storage = getStorage(app);
