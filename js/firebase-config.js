8c9d88752c269a39876b179fc216456a38b7c80b
smilefinanceAI <aarifkhankhatri00786@gmail.com>
2026-07-21 17:18:07 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,14 @@
+// ============================================================
+// Smile AI Web Studio
+// Firebase Configuration
+// ============================================================
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyArcvyvNAOKipPZB0YEfSeoeN9_k-8JznQ",
-  authDomain: "smile-business-os.firebaseapp.com",
-  projectId: "smile-business-os",
-  storageBucket: "smile-business-os.firebasestorage.app",
-  messagingSenderId: "114356400630",
-  appId: "1:114356400630:web:96fc9d07baaf3a504c17d7",
-  measurementId: "G-73WD9V05ZZ"
+    apiKey: "AIzaSyArcvyvNAOKipPZB0YEfSeoeN9_k-8JznQ",
+    authDomain: "smile-business-os.firebaseapp.com",
+    projectId: "smile-business-os",
+    storageBucket: "smile-business-os.firebasestorage.app",
+    messagingSenderId: "114356400630",
+    appId: "1:114356400630:web:96fc9d07baaf3a504c17d7",
+    measurementId: "G-73WD9V05ZZ"
 };
